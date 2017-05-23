63230bff9d98ea1b08d7ae8d8f3020ac317fb6fa
munnadroid <gotameraju2@gmail.com>
2017-05-23 15:06:38 +0545

    -application class added -kotlin android extension added

---
+++ app/src/main/java/com/awecode/stockapp/MyApplication.kt
@@ -0,0 +1,13 @@
+package com.awecode.stockapp
+
+import android.app.Application
+
+/**
+ * Created by munnadroid on 5/23/17.
+ */
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/awecode/stockapp/util/Util.kt
@@ -0,0 +1,5 @@
+package com.awecode.stockapp.util
+
+/**
+ * Created by munnadroid on 5/23/17.
+ */
