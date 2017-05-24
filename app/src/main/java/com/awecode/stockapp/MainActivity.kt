14229197c9fb4df2d9442c535abffa87038d0b66
munnadroid <gotameraju2@gmail.com>
2017-05-24 14:41:15 +0545

    vetctor drawables support added, market vector drawable added

--- app/src/main/java/com/awecode/stockapp/MainActivity.kt
+++ app/src/main/java/com/awecode/stockapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.awecode.stockapp
 
 import android.os.Bundle
+import android.support.v4.graphics.drawable.DrawableCompat
 import com.awecode.stockapp.view.base.BaseActivity
 import com.awecode.stockapp.view.home.market.MarketFragment
 import kotlinx.android.synthetic.main.activity_main.*
@@ -15,8 +16,12 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setSupportActionBar(toolbar)
         changeFragment(MarketFragment.newInstance(), true, false)
-        marketsButton.setOnClickListener({ toast("Button clicked") })
+        marketsButton.setOnClickListener({
+           toast("btn clicked")
+        })
 
     }
+
+
 }
 
--- app/src/main/java/com/awecode/stockapp/view/base/BaseActivity.kt
+++ app/src/main/java/com/awecode/stockapp/view/base/BaseActivity.kt
@@ -1,8 +1,10 @@
 package com.awecode.stockapp.view.base
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import com.awecode.stockapp.R
 
@@ -15,9 +17,15 @@ abstract class BaseActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(layoutId)
 
+
     }
 
     abstract val layoutId: Int
+
+
+    fun get_color(colorId: Int): Int {
+        return ContextCompat.getColor(applicationContext, colorId)
+    }
 
     fun changeFragment(fragment: Fragment, cleanStack: Boolean = false, addToBackStack: Boolean = true) {
         val ft = supportFragmentManager.beginTransaction()
