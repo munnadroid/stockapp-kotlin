7a9756abe400ddc6b321319c5d7e1412aa8a3879
munnadroid <gotameraju2@gmail.com>
2017-06-07 13:33:17 +0545

    - material form added - spinner added

--- app/src/main/java/com/awecode/stockapp/view/tools/ShareCalculatorFragment.kt
+++ app/src/main/java/com/awecode/stockapp/view/tools/ShareCalculatorFragment.kt
@@ -2,9 +2,10 @@ package com.awecode.stockapp.view.tools
 
 import android.os.Bundle
 import android.view.View
-import com.awecode.stockapp.MainActivity
+import android.widget.ArrayAdapter
 import com.awecode.stockapp.R
 import com.awecode.stockapp.view.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_share_calculator.*
 
 
 class ShareCalculatorFragment : BaseFragment() {
@@ -18,6 +19,15 @@ class ShareCalculatorFragment : BaseFragment() {
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupTypeSpinner()
+    }
+
+    private fun setupTypeSpinner() {
+        val txnTypeList = listOf("Buy", "Sell")
+        val adapter = ArrayAdapter(activity, R.layout.row_spn, txnTypeList)
+        adapter.setDropDownViewResource(R.layout.row_spn_dropdown)
+        txnTypeSpinnner.adapter = adapter
     }
 
     override fun fragmentVisible() {
